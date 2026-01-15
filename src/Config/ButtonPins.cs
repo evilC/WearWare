f0da1d1180315cbbafa8ba219667deb5531be47b
Clive Galway <evilc@evilc.com>
2026-01-15 13:39:13 +0000

    Set new pin numbers

--- src/Config/ButtonPins.cs
+++ src/Config/ButtonPins.cs
@@ -3,6 +3,7 @@ namespace WearWare.Config
     public static class ButtonPins
     {
         // public static IReadOnlyList<int> PinNumbers { get; } = new List<int> { 26, 21, 16, 14 };
-        public static IReadOnlyList<int> PinNumbers { get; } = new List<int> { 14, 16 };
+        // public static IReadOnlyList<int> PinNumbers { get; } = new List<int> { 14, 16 };
+        public static IReadOnlyList<int> PinNumbers { get; } = new List<int> { 2, 3, 16, 14 };
     }
 }
