1b77c0ddc1b0e06a5c02f3a1a5486af99a773c68
Clive Galway <evilc@evilc.com>
2026-01-29 06:01:16 +0000

    Move log to code-behind

---
+++ src/Components/Pages/Log/Log.razor.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Serilog.Events;
+using WearWare.Services.Logging;
+
+namespace WearWare.Components.Pages.Log
+{
+    public partial class Log
+    {
+        [Inject]
+        public InMemoryLogService LogService { get; set; } = null!;
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = null!;
+
+        private LogEventLevel _selectedLevel = LogEventLevel.Warning;
+        private LogEventLevel selectedLevel
+        {
+            get => _selectedLevel;
+            set
+            {
+                if (_selectedLevel != value)
+                {
+                    _selectedLevel = value;
+                    // Reload all entries at this level
+                    logEntries.Clear();
+                    lastIndex = 0;
+                    var allEntries = LogService.GetLogEvents(_selectedLevel, 0, out var newLastIndex);
+                    logEntries.AddRange(allEntries);
+                    lastIndex = newLastIndex;
+                    shouldScroll = true;
+                    StateHasChanged();
+                }
+            }
+        }
+        private List<LogEvent> logEntries = new();
+        private long lastIndex = 0;
+        private readonly LogEventLevel[] logLevels = (LogEventLevel[])Enum.GetValues(typeof(LogEventLevel));
+        private Timer? timer;
+        private ElementReference logEntriesDiv;
+        private bool shouldScroll = false;
+        private bool autoscrollEnabled = true;
+
+        protected override void OnInitialized()
+        {
+            timer = new Timer(_ =>
+            {
+                InvokeAsync(UpdateLog);
+            }, null, 0, 1000);
+        }
+
+        private void UpdateLog()
+        {
+            var newEntries = LogService.GetLogEvents(selectedLevel, lastIndex, out var newLastIndex);
+            if (newEntries.Count > 0)
+            {
+                logEntries.AddRange(newEntries);
+                lastIndex = newLastIndex;
+                shouldScroll = true;
+                StateHasChanged();
+            }
+        }
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (shouldScroll)
+            {
+                shouldScroll = false;
+                if (autoscrollEnabled)
+                {
+                    await JSRuntime.InvokeVoidAsync("logAutoscroll", logEntriesDiv);
+                }
+            }
+        }
+
+        private string FormatLogEntry(LogEvent entry)
+        {
+            return $"[{entry.Timestamp:HH:mm:ss} {entry.Level}] {entry.RenderMessage()}";
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
