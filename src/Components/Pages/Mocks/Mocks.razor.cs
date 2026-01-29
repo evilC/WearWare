b693ab9408cfaa28709a6b3d6e3189de6fae2639
Clive Galway <evilc@evilc.com>
2026-01-29 06:06:01 +0000

    Move mocks to code-behind

---
+++ src/Components/Pages/Mocks/Mocks.razor.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components;
+using WearWare.Services.MediaController;
+
+namespace WearWare.Components.Pages.Mocks
+{
+    public partial class Mocks : IDisposable
+    {
+        [Inject]
+        public QuickMediaService QuickMediaService { get; set; } = null!;
+        
+        [Inject]
+        public MediaControllerService MediaController { get; set; } = null!;
+        
+        protected override void OnInitialized()
+        {
+            QuickMediaService.StateChanged += OnStateChanged;
+        }
+
+        private void OnStateChanged()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            QuickMediaService.StateChanged -= OnStateChanged;
+        }
+    }
+}
