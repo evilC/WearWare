8915f88d13fe27ddeba1feca7ce1cc556762bfdd
Clive Galway <evilc@evilc.com>
2026-01-30 13:00:02 +0000

    Comments, add required properties to Model

--- src/Components/Forms/EditPlayableItemForm/EditPlayableItemFormModel.cs
+++ src/Components/Forms/EditPlayableItemForm/EditPlayableItemFormModel.cs
@@ -4,9 +4,21 @@ namespace WearWare.Components.Forms.EditPlayableItemForm
 {
     public class EditPlayableItemFormModel
     {
-        public EditPlayableItemFormMode FormMode { get; set; }
+        // When importing, this will be the new name
+        public string NewName { get; set; } = default!;
+        // The mode of the EditPlayableItemForm
+        public EditPlayableItemFormMode FormMode { get; set; } = default;
+        // The page that launched the EditPlayableItemForm
         public EditPlayableItemFormPage FormPage { get; set; } = default;
+        // In Add mode, the index to insert the new item at
+        // Or, in Add or Edit mode for QuickMedia items, the button number
         public int InsertIindex { get; set; } = default;
+        // In Add mode, the library item
+        // ToDo: Do we need both SelectedLibraryItem and OriginalItem?
         public PlayableItem SelectedLibraryItem { get; set; } = default!;
+        // The original item being edited
+        public PlayableItem OriginalItem { get; set; } = default!;
+        // The new or updated item
+        public PlayableItem UpdatedItem { get; set; } = default!;
     }
 }
