7a8300c79016e193bcb02c047f284848b8829402
Clive Galway <evilc@evilc.com>
2026-02-05 06:28:28 +0000

    Convert Mocks

--- src/Components/Pages/Mocks/Mocks.razor.cs
+++ src/Components/Pages/Mocks/Mocks.razor.cs
@@ -1,27 +1,39 @@
 using Microsoft.AspNetCore.Components;
+using WearWare.Components.Base;
 using WearWare.Services.MediaController;
 
 namespace WearWare.Components.Pages.Mocks
 {
-    public partial class Mocks : IDisposable
+    public partial class Mocks : DataPageBase
     {
         [Inject] public QuickMediaService QuickMediaService { get; set; } = null!;
         
         [Inject] public MediaControllerService MediaController { get; set; } = null!;
+        private bool _subscribed;
         
-        protected override void OnInitialized()
+        protected override Task InitializeDataAsync()
         {
-            QuickMediaService.StateChanged += OnStateChanged;
+            if (!_subscribed)
+            {
+                QuickMediaService.StateChanged += OnStateChanged;
+                _subscribed = true;
+            }
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            if (_subscribed)
+            {
+                QuickMediaService.StateChanged -= OnStateChanged;
+                _subscribed = false;
+            }
+            base.Dispose();
         }
 
         private void OnStateChanged()
         {
             InvokeAsync(StateHasChanged);
-        }
-
-        public void Dispose()
-        {
-            QuickMediaService.StateChanged -= OnStateChanged;
         }
     }
 }
