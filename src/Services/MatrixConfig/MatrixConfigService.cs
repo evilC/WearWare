6a0edc9447911a968ae6d4daced7cb9b4fa27014
Clive Galway <evilc@evilc.com>
2026-08-06 02:15:56 +0100

    Remove visibility options

--- src/Services/MatrixConfig/MatrixConfigService.cs
+++ src/Services/MatrixConfig/MatrixConfigService.cs
@@ -7,9 +7,7 @@ namespace WearWare.Services.MatrixConfig
     public class MatrixConfigService
     {
         private LedMatrixOptionsConfig _options;
-        private LedMatrixOptionsVisibility _visibility;
         private static readonly string ConfigFilePath = Path.Combine(PathConfig.ConfigPath, "matrixconfig.json");
-        private static readonly string VisibilityFilePath = Path.Combine(PathConfig.ConfigPath, "matrixconfig-visibility.json");
 
         public event Action? OptionsChanged;
         public event Action<int>? BrightnessChanged;
@@ -25,16 +23,6 @@ namespace WearWare.Services.MatrixConfig
             {
                 _options = JsonUtils.FromJsonFile<LedMatrixOptionsConfig>(ConfigFilePath) ?? new LedMatrixOptionsConfig();
             }
-            // Load visibility (UI-only flags) from a separate file
-            if (!File.Exists(VisibilityFilePath))
-            {
-                _visibility = new LedMatrixOptionsVisibility();
-                JsonUtils.ToJsonFile(VisibilityFilePath, _visibility);
-            }
-            else
-            {
-                _visibility = JsonUtils.FromJsonFile<LedMatrixOptionsVisibility>(VisibilityFilePath) ?? new LedMatrixOptionsVisibility();
-            }
         }
 
         public void UpdateOptions(LedMatrixOptionsConfig newOptions)
@@ -42,7 +30,6 @@ namespace WearWare.Services.MatrixConfig
             var oldOptions = _options.Clone();
             _options = newOptions;
             JsonUtils.ToJsonFile(ConfigFilePath, _options);
-            JsonUtils.ToJsonFile(VisibilityFilePath, _visibility);
 
             var brightnessChanged = oldOptions.Brightness != _options.Brightness;
             var oldWithoutBrightness = oldOptions.Clone();
@@ -58,14 +45,6 @@ namespace WearWare.Services.MatrixConfig
             {
                 OptionsChanged?.Invoke();
             }
-        }
-
-        public LedMatrixOptionsVisibility Visibility => _visibility;
-
-        public void UpdateVisibility(LedMatrixOptionsVisibility v)
-        {
-            _visibility = v ?? new LedMatrixOptionsVisibility();
-            JsonUtils.ToJsonFile(VisibilityFilePath, _visibility);
         }
 
         public LedMatrixOptionsConfig CloneOptions()
